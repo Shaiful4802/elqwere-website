bf40daf2ba0a588505bb4cd4b28dd68613594da1
Shaiful4802 <office.shaiful@gmail.com>
2024-09-17 00:38:05 +0600

    responsive udpdate

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 $(document).ready(function($){
     $('.counter').counterUp({
         delay: 10,
-        time: 2000,
+        time: 3000,
         offset: 70,
         beginAt: 10,
         formatter: function (n) {
